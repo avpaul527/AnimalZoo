5bb8a9b26c2800b3cf69df81e1b65cc1d7d87ec0
EthanTillmanAD <ethantillman101@gmail.com>
2024-10-24 14:14:57 -0400

    branch_added

--- src/main/java/zoo/Cat.java
+++ src/main/java/zoo/Cat.java
@@ -1,4 +1,13 @@
 package zoo;
 
-public class Cat {
+public class Cat extends Feline implements Pet{
+
+    public void makeNoise(){
+        System.out.println("Meow Meow");
+    }
+
+    @Override
+    public boolean isAPet() {
+        return true;
+    }
 }
--- src/main/java/zoo/Pet.java
+++ src/main/java/zoo/Pet.java
@@ -1,4 +1,5 @@
 package zoo;
 
 public interface Pet {
+   boolean isAPet();
 }
